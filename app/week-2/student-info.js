9099355de901b8357cc2aa94774909685ea4ff0c
derek-du-forever <guanmeng.du@edu.sait.ca>
2025-09-11 11:15:57 -0600

    Refactor Home page layout and add Week 2 Assignment link; include StudentInfo component in Shopping List page

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,13 @@ export function StudentInfo() {
     return (
         <div>
             <label className="mr-2.5">Guanmeng Du</label>
-            <Link href=""></Link>
+            <Link
+                className="underline text-blue-500"
+                target="_blank"
+                href="https://github.com/derek-du-forever/cprg306-assignments.git"
+            >
+                Goto My Github
+            </Link>
         </div>
     );
 }
