9eb294230b1b3409234ffec335749f84f960b103
derek-du-forever <guanmeng.du@edu.sait.ca>
2025-09-11 10:55:05 -0600

    modified:   app/layout.js 	modified:   app/page.js 	new file:   app/week-2/page.js 	new file:   app/week-2/student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+    return (
+        <div>
+            <label className="mr-2.5">Guanmeng Du</label>
+            <Link href=""></Link>
+        </div>
+    );
+}
