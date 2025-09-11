e67dae116aa28195ab0a383f293164a172d5881c
derek-du-forever <guanmeng.du@edu.sait.ca>
2025-09-11 13:09:55 -0600

    Add padding to main container in Home page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main>
+        <main className="p-2">
             <h1>CPRG 306: Web Development 2 - Assignments</h1>
 
             <div>
